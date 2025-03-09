bd6a4a22ba44a2f4387404accff7523af342d59d
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2025-03-09 17:28:40 -0500

    First request and set up the proxy

--- src/app/employees-list/employees-list.component.ts
+++ src/app/employees-list/employees-list.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { EmployeesTableComponent } from "../employees-table/employees-table.component";
+import { EmployeeService } from '../service/employee.service';
 
 @Component({
   selector: 'app-employees-list',
-  imports: [],
+  imports: [EmployeesTableComponent],
   templateUrl: './employees-list.component.html',
   styleUrl: './employees-list.component.css'
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit 
+{
+  private employeeService = inject(EmployeeService);
 
+  ngOnInit(): void {
+    this.employeeService.getAllEmployees()
+      .subscribe(
+        {
+          next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });  
+  }
 }
--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private baseUrl = "/api/employees"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllEmployees()
+  {
+    return this.http.get<any>(`${this.baseUrl}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  public handleError(error: HttpErrorResponse)
+  {
+    if (error.status === 0)
+      console.error("There was an error:\n\t", error.error);
+    else
+    {
+      console.error("The Backend returned the code: " 
+        + error.status + "\nBody: \n\t" + error.error.errors);
+    }
+
+    return throwError(() => error);
+  }
 }
