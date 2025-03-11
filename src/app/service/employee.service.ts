8712de048884a6e2fffd6eef8db0bf95396fe00d
Felipe Zea <48108543+Felipe27200@users.noreply.github.com>
2025-03-10 22:07:47 -0500

    Set environments and deploy

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, throwError } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private baseUrl = "/api/employees"
+  private baseUrl = environment.apiUrl + "/api/employees";
 
   constructor(
     private http: HttpClient
